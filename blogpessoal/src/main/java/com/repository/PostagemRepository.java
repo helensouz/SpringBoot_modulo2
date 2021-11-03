ea76e011050efe1caca190d9e47c7a1d66ffd217
Helen Oliveira <65935692+helensouz@users.noreply.github.com>
2021-11-03 18:41:08 -0300

    blogpessoal com spring

---
+++ blogpessoal/src/main/java/com/repository/PostagemRepository.java
@@ -0,0 +1,16 @@
+package com.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.model.Postagem;
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+	public List<Postagem> findAllByTituloContainingInorecase(String titulo);
+
+}
